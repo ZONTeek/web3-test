a4a5a114167d8fffcafa2454ef2741e46fc7e3b6
ZONTeek <Platon.Boyko@outlook.com>
2022-03-31 21:11:32 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
+import "./App.css";
 
 function App() {
+  const [account, setAccount] = useState("");
+
+  useEffect(() => {
+    (async function load() {
+      const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+      const accounts = await web3.eth.requestAccounts();
+
+      setAccount(accounts[0]);
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login-info">
+        {account ? (
+          <div>Your public key is {account}</div>
+        ) : (
+          <p>MetaMask is locked - please login</p>
+        )}
+      </div>
     </div>
   );
 }
